3d05653db6afcdcfc4bcb13e962ff45e44c7434b
Austin Smith <asmit2222@gmail.com>
2020-04-30 16:13:15 -0400

    added seed script and seed file

--- server/scripts/seed.js
+++ server/scripts/seed.js
@@ -10,12 +10,14 @@ function getRandomInt(max) {
 }
 
 for (var i = 0; i < 30; i++) {
+
 var insertStatement = `INSERT INTO reviews(id, name, stars, date, review, image)
 VALUES(${getRandomInt(5)}, '${faker.name.firstName()} ${faker.name.lastName()}', '${getRandomInt(2) + 4}', 'Reviewed in the United States on ${faker.date.month()} ${getRandomInt(29) + 1} ${getRandomInt(2) + 2018}', '${faker.lorem.sentences()}', 'randomURL')`;
 
     db.connection.query(insertStatement, function (err, result) {
      console.log('inserted')
     });
+
   };
 
 
