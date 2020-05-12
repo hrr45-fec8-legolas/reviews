f2c2a22b8fccc10bb9b487e3c28a34b323d738ff
Austin Smith <asmit2222@gmail.com>
2020-05-12 13:57:02 -0400

    fixed bug where it breaks on localhost without id

--- database/index.js
+++ database/index.js
@@ -4,7 +4,13 @@ const mariadbConfig = require("./config.js");
 const connection = mariadb.createConnection(mariadbConfig);
 
 const getAllReviews = function(id, callback) {
-  var sql = `select * from reviews where id=${id}`;
+  console.log(id);
+  if (id === null) {
+    var sql = `select * from reviews where id=${1}`;
+  } else {
+    var sql = `select * from reviews where id=${id}`;
+  }
+  console.log(sql);
   connection.query(sql, function(err, results) {
     if (err) {
       callback(err);
--- server/index.js
+++ server/index.js
@@ -1,27 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('../database');
+const express = require("express");
+const bodyParser = require("body-parser");
+const db = require("../database");
 
 const app = express();
 const PORT = 3004;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(__dirname + "/../client/dist"));
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
-app.get('/api/allreviews/', (req, res) => {
-  var arr = req._parsedOriginalUrl.search.split('=');
+app.get("/api/allreviews/", (req, res) => {
+  if (req._parsedOriginalUrl.search) {
+    var arr = req._parsedOriginalUrl.search.split("=");
+  }
   // console.log('id server: ', arr[1]);
-  db.getAllReviews(arr[1], (err, data) => {
-    if(err) {
-     res.status(500).send('Something Broke!');
-    } else {
-    res.json(data)
-    }
-  })
- });
-
+  if (arr) {
+    db.getAllReviews(arr[1], (err, data) => {
+      if (err) {
+        res.status(500).send("Something Broke!");
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    db.getAllReviews(null, (err, data) => {
+      if (err) {
+        res.status(500).send("Something Broke!");
+      } else {
+        res.json(data);
+      }
+    });
+  }
+});
