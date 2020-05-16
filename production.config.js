274b21a4097002112a96e78511646f63680dfeec
Austin Smith <asmit2222@gmail.com>
2020-05-16 13:50:27 -0400

    production.config file

---
+++ production.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./client/index.jsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.m?jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-react",
+              "@babel/preset-env",
+              "babel-preset-airbnb",
+            ],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, "client/dist"),
+    filename: "bundle.js",
+  },
+};
